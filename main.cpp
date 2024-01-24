12c0ccb2555c114887ccec61b4b13485d2cdc697
Collin Harrington <collin1234harr@gmail.com>
2024-01-24 19:29:19 +0000

    First version of program

---
+++ main.cpp
@@ -0,0 +1,48 @@
+/*! @mainpage Automatic Headlight Control
+* @date Wednesday, January 24, 2024
+* @authors Collin Harrington, ***********
+* @section genDesc General Description
+*
+*
+* This is the implementation of a system that prototypes
+* automatic headlight control for motor vehicles. 
+*
+* @section changelog Changelog
+* | Date | Description |
+* |:----------:|:-----------------------------------------------|
+* | 01/24/2024 | First version of program |
+*
+*
+*/
+
+//=====[Libraries]=============================================================
+
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+//=====[Declaration and initialization of public global objects]===============
+
+//=====[Declaration and initialization of public global variables]=============
+
+//=====[Declaration and initialization of public global types]=================
+
+//=====[Main function, the program entry point after power on or reset]========
+
+/**
+* Calls functions to initialize the declared input and output objects, and to
+* implement the system behavior.
+* @param none
+* @return The returned value represents the success
+* of application.
+*/
+
+int main()
+{
+    while (true) {
+
+    }
+}
+
+//=====[Implementation of global functions]====================================
+
+
